11ecb40df10987c5232fc32bccc614b915bfe4eb
nindozzmo <80521361+nindozzmo@users.noreply.github.com>
2022-05-07 23:11:33 +0300

    Add files via upload

--- js/convert.js
+++ js/convert.js
@@ -5,67 +5,86 @@ window.onload = function() {
   const left = document.getElementById("left");
   const right = document.getElementById("right");
 
-  fahrenheit.addEventListener('input', fahrenheitToCelsius => {
-    
-    function fToC (){
-      let c = (fahrenheit.value - 32) / 1.8;
+  fahrenheit.addEventListener('input', function () {
+
+    function fahrToCels(x) {
+      let c = (x - 32) / 1.8;
       celsius.value = c.toFixed(1);
       left.innerHTML = "";
       right.innerHTML = "";
     }
-    
+
+    function fahrLenCelsRight(x) {
+      if (x == "" || x == "-" || isNaN(x)) {
+        fahrenheit.setAttribute("maxlength", "4");
+        celsius.value = "";
+        right.innerHTML = "";
+      }
+    }
+
+    function setLength(x) {
+      if (x < 0) {
+        fahrenheit.setAttribute("maxlength", "5");
+      }
+      else if (x > 0 || x == 0) {
+        fahrenheit.setAttribute("maxlength", "4");
+      }
+    }
+
     if (fahrenheit.value == "" || fahrenheit.value == "-") {
-      fahrenheit.setAttribute("maxlength", "4");
-      celsius.value = "";
+      fahrLenCelsRight(fahrenheit.value);
       left.innerHTML = "";
-      right.innerHTML = "";
     }
     else if (isNaN(fahrenheit.value)) {
-      fahrenheit.setAttribute("maxlength", "4");
-      celsius.value = "";
+      fahrLenCelsRight(fahrenheit.value);
       fahrenheit.value = "";
       left.innerHTML = "invalid input";
-      right.innerHTML = "";
     }
-    else if (fahrenheit.value < 0) {
-      fahrenheit.setAttribute("maxlength", "5");
-      fToC(fahrenheit.value);
-    }
-    else if (fahrenheit.value > 0 || fahrenheit.value == 0) {
-      fahrenheit.setAttribute("maxlength", "4");
-      fToC(fahrenheit.value);
+    else {
+      setLength(fahrenheit.value);
+      fahrToCels(fahrenheit.value);
     }
   })
+  
+  celsius.addEventListener('input', function () {
 
-  celsius.addEventListener('input', celsiusToFahrenheit => {
-    
-    function cToF (){
-      let f = celsius.value * 1.8 + 32;
+    function celsTofahr(x) {
+      let f = x * 1.8 + 32;
       fahrenheit.value = f.toFixed(1);
       left.innerHTML = "";
       right.innerHTML = "";
     }
-    
+
+    function celsLenFahrLeft(x) {
+      if (x == "" || x == "-" || isNaN(x)) {
+        celsius.setAttribute("maxlength", "4");
+        fahrenheit.value = "";
+        left.innerHTML = "";
+      }
+    }
+
+    function setLength(x) {
+      if (x < 0) {
+        celsius.setAttribute("maxlength", "5");
+      }
+      else if (x > 0 || x == 0) {
+        celsius.setAttribute("maxlength", "4");
+      }
+    }
+
     if (celsius.value == "" || celsius.value == "-") {
-      celsius.setAttribute("maxlength", "4");
-      fahrenheit.value = "";
-      left.innerHTML = "";
+      celsLenFahrLeft(celsius.value);
       right.innerHTML = "";
     }
     else if (isNaN(celsius.value)) {
-      celsius.setAttribute("maxlength", "4");
-      fahrenheit.value = "";
+      celsLenFahrLeft(celsius.value);
       celsius.value = "";
       right.innerHTML = "invalid input";
-      left.innerHTML = "";
     }
-    else if (celsius.value < 0) {
-      celsius.setAttribute("maxlength", "5");
-      cToF(celsius.value);
-    }
-    else if (celsius.value > 0 || celsius.value == 0) {
-      celsius.setAttribute("maxlength", "4");
-      cToF(celsius.value);
+    else {
+      setLength(celsius.value);
+      celsTofahr(celsius.value);
     }
   })
+
 }
