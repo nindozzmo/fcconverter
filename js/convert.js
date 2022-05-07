bc5d957cee8004f5f8bb1a7483f77eedd2230c26
nindozzmo <80521361+nindozzmo@users.noreply.github.com>
2022-05-07 15:13:30 +0300

    Add files via upload

--- js/convert.js
+++ js/convert.js
@@ -1,15 +1,19 @@
 window.onload = function() {
       
   const fahrenheit = document.getElementById("fahrenheit");
-  const celsius = document.getElementById("celsius");  
+  const celsius = document.getElementById("celsius");
 
   fahrenheit.addEventListener('input', fahrenheitToCelsius => {
-    let c = (fahrenheit.value - 32) / 1.8;
     
-    if (fahrenheit.value == "") {
+    if (fahrenheit.value == "" || fahrenheit.value == "-") {
       celsius.value = "";
     }
+    else if (isNaN(fahrenheit.value)) {
+      celsius.value = "";
+      fahrenheit.value = "";
+    }
     else {
+      let c = (fahrenheit.value - 32) / 1.8;
       celsius.value = c.toFixed(1);
     }
   })
@@ -17,12 +21,15 @@ window.onload = function() {
   celsius.addEventListener('input', celsiusToFahrenheit => {
     let f = celsius.value * 1.8 + 32;
 
-    if (celsius.value == "") {
+    if (celsius.value == "" || celsius.value == "-") {
       fahrenheit.value = "";
+    }
+    else if (isNaN(celsius.value)) {
+      fahrenheit.value = "";
+      celsius.value = "";
     }
     else {
       fahrenheit.value = f.toFixed(1);
     }
   })
-  
 }
