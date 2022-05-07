79b25a0477625a05a245ad6a1d6e18dc643ee342
nindozzmo <80521361+nindozzmo@users.noreply.github.com>
2022-05-07 04:41:25 +0300

    Add files via upload

--- js/convert.js
+++ js/convert.js
@@ -5,12 +5,24 @@ window.onload = function() {
 
   fahrenheit.addEventListener('input', fahrenheitToCelsius => {
     let c = (fahrenheit.value - 32) / 1.8;
-    celsius.value = c.toFixed(1);    
+    
+    if (fahrenheit.value == "" || fahrenheit.value == "-") {
+      celsius.value = "";
+    }
+    else {
+      celsius.value = c.toFixed(1);
+    }
   })
   
   celsius.addEventListener('input', celsiusToFahrenheit => {
     let f = celsius.value * 1.8 + 32;
-    fahrenheit.value = f.toFixed(1);
+
+    if (celsius.value == "" || celsius.value == "-") {
+      fahrenheit.value = "";
+    }
+    else {
+      fahrenheit.value = f.toFixed(1);
+    }
   })
   
 }
